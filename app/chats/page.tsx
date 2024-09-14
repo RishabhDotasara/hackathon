2e39f6476d5693d5c6f5f1c9985e1985488e74b9
AST <ashwajittayade30@gmail.com>
2024-09-14 08:15:34 +0530

    first commit

---
+++ app/chats/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Test from '../components/Test'
+import { Button } from '@/components/ui/button'
+const page = () => {
+  return (
+    <div>
+      This is Chat app
+      <Button />
+    </div>
+  )
+}
+
+export default page
