b297274006d390396bedd40421eb2a68d40f8413
Rishabh Dotasara <rishabhdotasara@gmail.com>
2024-10-23 02:05:26 +0530

    solved the full route caching issue

--- app/api/teams/create/route.ts
+++ app/api/teams/create/route.ts
@@ -1,6 +1,7 @@
 import { PrismaClient, User } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
 export async function POST(request: NextRequest) {
   try {
     const prisma = new PrismaClient();
--- app/api/teams/delete/route.ts
+++ app/api/teams/delete/route.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 
+export const dynamic = 'force-dynamic'
 export async function DELETE(req:NextRequest)
 {
     try 
